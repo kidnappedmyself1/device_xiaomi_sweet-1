97345674b858ec3834fa5f334793d22d55863707
itsxrp <itsxrproms@gmail.com>
2021-10-29 13:12:59 +0000

    sweet: Bring Up Project Elixir Official
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_sweet.mk
+    $(LOCAL_DIR)/aosp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_sweet-eng \
-    fluid_sweet-userdebug \
-    fluid_sweet-user
+    aosp_sweet-eng \
+    aosp_sweet-userdebug \
+    aosp_sweet-user
--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -5,15 +5,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit Project Fluid product configuration
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit Project Elixir product configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-# Inherit Project Fluid Official build stuff.
-FLUID_BUILD_TYPE := TEST
 
-PRODUCT_NAME := fluid_sweet
+# Inherit Project Elixir Props
+IS_PHONE := true
+CUSTOM_BUILD_TYPE=OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
