6a2c1b81eec8f335faf8f1b792e5526ee0c3fc46
itsxrp <itsxrproms@gmail.com>
2022-01-16 06:44:17 +0000

    sweet: Switch to Raven Jan 2022 FP
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -28,6 +28,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys
