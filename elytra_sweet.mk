6d7f85a4685de01c8fe54f94c097b6a9d31d144a
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-30 09:04:39 +0530

    bringup elytra

--- elytra_sweet.mk
+++ elytra_sweet.mk
@@ -6,20 +6,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit Project Elixir product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/elytra/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit Project Elixir Props
-Elixir_MAINTAINER := X•R•P
-IS_PHONE := true
-CUSTOM_BUILD_TYPE=OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := elytra_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
