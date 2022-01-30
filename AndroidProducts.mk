c27d3ad8c54fb9ac12b2b3bafb27bfc524ad973b
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-30 09:03:29 +0530

    bringup elytra

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/elytra_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-eng \
-    lineage_sweet-userdebug \
-    lineage_sweet-user
+    elytra_sweet-eng \
+    elytra_sweet-userdebug \
+    elytra_sweet-user
